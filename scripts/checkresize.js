c572235e58a9a29e412095ad59e9e2042fab77d2
Fazenz <liliankb@icloud.com>
2024-12-03 02:03:42 +0100

    Added fonts to index

--- scripts/checkresize.js
+++ scripts/checkresize.js
@@ -6,40 +6,36 @@ function checkResize() {
         if (!hasAnimated) {
             const elements = document.querySelectorAll('.name-wrapper, .id-wrapper, .email-wrapper, .password-wrapper, .submit-button, .confirm-password-wrapper, .validation-wrapper, .email-text');
             elements.forEach(el => {
-                el.style.animation = 'widthchangeback 0.24s forwards'; // Ajout de l'animation
+                el.style.animation = 'widthchangeback 0.24s forwards';
             });
             hasAnimated = true;
         }
     } else if (window.innerWidth <= 1400) {
-        // Si la largeur devient inférieure ou égale à 1400px, réinitialiser l'animation si nécessaire
         hasAnimated = false; 
     } else {
-        hasAnimated = false; // Réinitialise si la taille sort de la plage entre 1400 et 1450px
+        hasAnimated = false;
     }
 }
 
 function checkResize2() {
     if (window.innerWidth <= 1400) {
-        if (!hasAnimated2) {  // Vérifie si l'animation n'a pas encore été lancée
+        if (!hasAnimated2) { 
             const elements = document.querySelectorAll('.name-wrapper, .id-wrapper, .email-wrapper, .password-wrapper, .submit-button, .confirm-password-wrapper, .validation-wrapper, .email-text');
             elements.forEach(el => {
-                el.style.animation = 'widthchange 0.24s forwards'; // Ajout de l'animation
+                el.style.animation = 'widthchange 0.24s forwards';
             });
-            hasAnimated2 = true; // Marque que l'animation a été effectuée
+            hasAnimated2 = true;
         }
     } else {
-        hasAnimated2 = false;  // Réinitialisation pour permettre la relance
+        hasAnimated2 = false;
     }
 }
 
-// Fonction de détection précise de la largeur
 function handleResize() {
     checkResize();
     checkResize2();
 }
 
-// Écoute l'événement de redimensionnement
 window.addEventListener('resize', handleResize);
 
-// Vérifie lors du chargement initial de la page
 handleResize();
